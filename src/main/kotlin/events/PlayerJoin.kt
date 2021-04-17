4e3c98d2bbc85e7509fc415017fef828ed8d201b
SmushyTaco <41648290+realSmushyTaco@users.noreply.github.com>
2021-04-17 15:26:00 -0700

    Removes deprecated methods

--- src/main/kotlin/events/PlayerJoin.kt
+++ src/main/kotlin/events/PlayerJoin.kt
@@ -1,5 +1,6 @@
 package events
 
+import net.kyori.adventure.text.Component
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
@@ -9,6 +10,7 @@ class PlayerJoin : Listener {
     @EventHandler
     fun playerJoin(event: PlayerJoinEvent) {
         val player: Player = event.player
-        event.joinMessage = "Hello ${player.displayName}!!!"
+        event.joinMessage(Component.text("Hello ").append(player.displayName())
+            .append(Component.text("!!!")))
     }
 }
